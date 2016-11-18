05d70f7a63416e20c6cf60451a4b856473bdb044
Michael Bischoff <Michael Bischoff@DESKTOP-A963I7O>
2016-11-18 14:43:08 +0100

    Erster Test der Git-Funktionalitäten

---
+++ Notenverwaltung/src/de/tum/sep/siglerbischoff/notenverwaltung/controller/Main.java
@@ -0,0 +1,69 @@
+package de.tum.sep.siglerbischoff.notenverwaltung.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import de.tum.sep.siglerbischoff.notenverwaltung.dao.DAO;
+import de.tum.sep.siglerbischoff.notenverwaltung.dao.DatenbankFehler;
+import de.tum.sep.siglerbischoff.notenverwaltung.model.Benutzer;
+import de.tum.sep.siglerbischoff.notenverwaltung.view.LoginView;
+import de.tum.sep.siglerbischoff.notenverwaltung.view.View;
+
+public final class Main implements ActionListener {
+
+	private static final boolean debug = true;
+	
+	private View view;
+	private DAO dao;
+	
+	private Benutzer loggedIn;
+	
+	public static void main(String[] args) {
+		new Main();
+	}
+	
+	private Main() {
+		view = View.erstelleView();
+		view.addActionListener(this);
+		
+		try {
+			dao = DAO.erstelleDAO();
+			
+			login();
+		} catch (DatenbankFehler e) {
+			if(debug) {
+				e.printStackTrace();
+			}
+			view.showError("Datenbankfehler", "Fehler beim Verbinden mit der Datenbank, prüfen Sie Ihre Internetverbindung. ");
+			System.exit(0);
+		}
+	}
+	
+	private void login() {
+		LoginView lv = view.getLoginView();
+		lv.addLoginListener(ae -> {
+			try {
+				Benutzer benutzer = dao.passwortPruefen(lv.getUser(), lv.getPassword());
+				if(benutzer == null) {
+					lv.failure();
+				} else {
+					loggedIn = benutzer;
+					lv.success();
+					view.loginBenutzer(loggedIn);
+				}
+			} catch (DatenbankFehler e) {
+				if(debug) {
+					e.printStackTrace();
+				}
+				view.showError(e);
+			}
+		});
+		lv.login();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+}
